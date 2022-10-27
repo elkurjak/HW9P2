52242c9b0fef447548d1db3830cc55a7186c2dc2
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-10-27 05:29:28 +0000

    turned it  into a function

--- .ccls-cache/@home@runner@9-2/main.cpp
+++ .ccls-cache/@home@runner@9-2/main.cpp
@@ -2,15 +2,23 @@
 #include <iomanip>
 
 using namespace std;
+void printMatrix(int grades[][5], int ROWS, int COLUMNS);
 
 int main() {
 //declare constants for my number of cols/rows
   const int COLUMNS = 5;
   const int ROWS = 5;
+  
   //input grades
   int grades [ROWS] [COLUMNS] = {{1, 100,100,100,100}, {2,100,0,100, 0}, {3,82,94,73,86},{4,64,74,84,94}, {5,94,84,74,64}};
-//for loops
-for(int a = 0; a < 5; a++){
+  
+ printMatrix(grades, ROWS, COLUMNS);
+  
+  return 0;
+}
+
+void printMatrix(int grades[][5], int ROWS, int COLUMNS) {
+  for(int a = 0; a < 5; a++){
     for(int b = 0; b < 5; b++) {
       cout << setw(5)<< grades[a][b] ;
       }
@@ -34,4 +42,4 @@ for(int a = 0; a < 5; a++){
     cout << setw(5)<< average2;
     cout<< endl;
 }
-}
+  }
